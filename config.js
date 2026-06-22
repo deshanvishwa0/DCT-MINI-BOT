d2dd5faa247ed608acb74b652769f5a9acd1e044
deshanvishwa0 <deshanvishwa062@gmail.com>
2026-06-22 13:58:24 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,23 @@
+const BOT_NAME_FANCY = '✦ ━━ DCT MINI BOT ━━ ✦';
+
+const config = {
+  AUTO_VIEW_STATUS: 'true',
+  AUTO_LIKE_STATUS: 'true',
+  AUTO_RECORDING: 'false',
+  AUTO_LIKE_EMOJI: ['☘️','💗','🫂','🙈','🍁','🙃','🧸','😘','🏴‍☠️','👀','❤️‍🔥'],
+  PREFIX: '.',
+  MAX_RETRIES: 3,
+  GROUP_INVITE_LINK: 'https://chat.whatsapp.com/I7eReF73VGu4EU9kF9w4uS?mode=gi_t',
+  IMAGE_PATH: 'https://files.catbox.moe/x54ibb.jpg',
+  NEWSLETTER_JID: '120363421785026867@newsletter',
+  OTP_EXPIRY: 300000,
+  WORK_TYPE: 'public',
+  OWNER_NUMBER: process.env.OWNER_NUMBER || '94781617255',
+  CHANNEL_LINK: 'https://whatsapp.com/channel/0029Vb7p3UCCHDyocfEGm23f',
+  BOT_NAME: '✦ ━━ DCT MINI BOT ━━ ✦',
+  BOT_VERSION: '1.0.0V',
+  OWNER_NAME: '𝙳𝙰𝙼𝙸𝚃𝙷 𝙼𝙰𝙳𝚄𝚂𝙰𝙽𝙺𝙰',
+  IMAGE_PATH: 'https://files.catbox.moe/cy9eam.jpeg',
+  BOT_FOOTER: '> *©ᴘᴏᴡᴇʀᴅ ʙʏ ⚔️ ||DCT MINI BOT|| 🐉*',
+  BUTTON_IMAGES: { ALIVE: 'https://files.catbox.moe/x54ibb.jpg }
+};
